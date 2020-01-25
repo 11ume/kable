7bc6dd6c8e0ac4b5425b962a635d326aac0257d6
hume <hume@hume@23.org>
2020-01-25 18:34:13 -0300

    add metadata to test

--- test/kable.test.ts
+++ test/kable.test.ts
@@ -5,9 +5,13 @@ test.serial('check normal functionalities, create node and pick', async (t) => {
     const id = 'bar'
     const host = '192.168.0.1'
     const port = 25565
+    const meta = {
+        id: 'service-bar'
+        , description: 'cool service'
+    }
 
     const foo = kable('foo')
-    const bar = kable(id, { host, port })
+    const bar = kable(id, { host, port, meta })
 
     await foo.up()
     await bar.up()
@@ -16,6 +20,7 @@ test.serial('check normal functionalities, create node and pick', async (t) => {
     t.is(check.id, id)
     t.is(check.host, host)
     t.is(check.port, port)
+    t.deepEqual(check.meta, meta)
 
     foo.down()
     bar.down()
