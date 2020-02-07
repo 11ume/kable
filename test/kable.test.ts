d8d800cf94b9e185c5474dea6f37b5002250bee1
11ume <horacioriverods@gmail.com>
2020-02-07 13:19:48 -0300

    add test

--- test/kable.test.ts
+++ test/kable.test.ts
@@ -1,7 +1,21 @@
 import test from 'ava'
 import kable from '../main'
 
-test.serial('check normal functionalities, create node and pick', async (t) => {
+test.serial('check normal functionalities, create node and pick one', async (t) => {
+    const foo = kable('foo')
+    const bar = kable('bar')
+
+    await foo.up()
+    await bar.up()
+
+    const check = await foo.pick('bar')
+    t.is(check.id, 'bar')
+
+    foo.down()
+    bar.down()
+})
+
+test.serial('check normal functionalities, create node and pick one, whit custom host, port, metadata', async (t) => {
     const id = 'bar'
     const host = '192.168.0.1'
     const port = 25565
